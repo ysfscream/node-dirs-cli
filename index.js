85cd48aa86d755b6f1f37af86d5b51ca77857f35
ysfscream <894402575@qq.com>
2019-01-15 14:21:17 +0800

    :sparkles: Complete base feature

--- index.js
+++ index.js
@@ -4,28 +4,51 @@
 const fs = require('fs')
 const path = require('path')
 const signale = require('signale')
+const argv = require('yargs')
+  .alias('p', 'path')
+  .alias('n', 'filename')
+  .alias('e', 'export')
+  .alias('f', 'filter')
+  .array('filter')
+  .argv
 
-let _dirname = './';
-if (process.argv[2]) {
-  _dirname = process.argv[2];
-};
+let [_dirname, _filename, _export, _filter] = ['./', 'data.json', './', null]
+
+if (argv.path) {
+  _dirname = argv.path
+}
+if (argv.filename) {
+  _filename = argv.filename
+}
+if (argv.export) {
+  _export = argv.export
+}
+if (argv.filter) {
+  _filter = argv.filter
+}
 
 const root = path.join(_dirname)
 
-const getDirs = (path, filter) => {
-  return fs.readdirSync(path)
+const getDirs = (path) => {
+  const dirs = fs.readdirSync(path)
+  if (_filter) {
+    return dirs.filter((dir) =>
+      !_filter.includes(dir)
+    )
+  }
+  return dirs
 }
 
 const setApps = (dirs) => {
-  console.log(dirs)
-  signale.success('Get directory of apps successful!')
-// fs.writeFile('./src/assets/micro.apps.json', JSON.stringify(dirs), (err) => {
-//   if (err) {
-//     console.log(err)
-//   } else {
-//     console.log('Get directory of apps successful!')
-//   }
-// })
+  fs.writeFile(`${_export}/${_filename}`,
+    JSON.stringify(dirs),
+    (err) => {
+      if (err) {
+        signale.error(err)
+      } else {
+        signale.success('Get directory of apps successful!')
+      }
+    })
 }
 
 setApps(getDirs(root))
